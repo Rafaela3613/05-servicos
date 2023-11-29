5daca5375d02d62907b0e49512563d36c285b88f
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-11-29 10:30:12 -0300

    incluindo injecao de dependencia

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -12,10 +12,10 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: string[]= [];
-  cursosService: CursosService;
+  //cursosService: CursosService;
 
-  constructor() {
-    this.cursosService = new CursosService();
+  constructor(private cursosService: CursosService) {
+    //this.cursosService = _cursosService;
   }
 
   ngOnInit(): void {
