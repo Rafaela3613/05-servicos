fbbdae742d79cb70b94a136036b94c06b0f4277c
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-12-04 10:47:09 -0300

    cominicacao entre componentes usando servicos

--- src/app/criar-curso/criar-curso.component.ts
+++ src/app/criar-curso/criar-curso.component.ts
@@ -5,21 +5,21 @@ import { CursosService } from '../cursos/cursos.service';
 @Component({
   selector: 'app-criar-curso',
   templateUrl: './criar-curso.component.html',
-  styleUrls: ['./criar-curso.component.css']
+  styleUrls: ['./criar-curso.component.css'],
+  providers: [CursosService]
 })
 export class CriarCursoComponent implements OnInit {
 
-  cursos: string []=[];
-  cursosService: any;
+  cursos: string[] = [];
 
-  constructor(private CursosService: CursosService ) { }
+  constructor(private cursosService: CursosService) { }
 
-  ngOnInit(): void {
-    this.cursos = this.CursosService.getCursos();
-
+  ngOnInit() {
+    this.cursos = this.cursosService.getCursos();
   }
-  onAddCurso(curso:string){
-      this.cursosService.addCurso(curso);
+
+  onAddCurso(curso: string){
+    this.cursosService.addCurso(curso);
   }
 
 }
--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -2,24 +2,28 @@ import { Component, OnInit } from '@angular/core';
 
 import { CursosService } from './cursos.service';
 
-
-
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
+  providers: [CursosService]
 })
 export class CursosComponent implements OnInit {
 
-  cursos: string[]= [];
+  cursos: string[] = [];
   //cursosService: CursosService;
 
   constructor(private cursosService: CursosService) {
+    //this.cursosService = new CursosService();
     //this.cursosService = _cursosService;
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.cursos = this.cursosService.getCursos();
+
+    CursosService.criouNovoCurso.subscribe(
+      curso => this.cursos.push(curso)
+    );
   }
 
 }
